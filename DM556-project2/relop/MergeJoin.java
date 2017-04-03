de5fe72c22fceb8f26e20c4dc64d4a1780cacb9c
Mark Jervelund <mark@jervelund.com>
2017-04-03 12:13:52 +0200

    Fixed merge join.

--- DM556-project2/relop/MergeJoin.java
+++ DM556-project2/relop/MergeJoin.java
@@ -6,131 +6,130 @@ import java.util.IllegalFormatException;
 public class MergeJoin extends Iterator {
 
 
-	/** The underlying left iterator. */
-	protected Iterator left;
+    /**
+     * The underlying left iterator.
+     */
+    protected Iterator left;
 
-	/** The underlying right iterator. */
-	protected Iterator right;
+    /**
+     * The underlying right iterator.
+     */
+    protected Iterator right;
 
-	/** left col. */
-	protected Integer lcol;
+    /**
+     * left col.
+     */
+    protected Integer lcol;
 
-	/** right col. */
-	protected Integer rcol;
+    /**
+     * right col.
+     */
+    protected Integer rcol;
 
-	/** Current tuple from left iterator. */
-	protected Tuple tuple;
+    /**
+     * Current tuple from left iterator.
+     */
+    protected Tuple tuple;
 
-	/** Current tuple from left iterator. */
-	protected Tuple outer;
+    /**
+     * Current tuple from left iterator.
+     */
+    protected Tuple outer;
 
 
-	/** Next tuple to return. */
-	protected Tuple next;
+    /**
+     * Next tuple to return.
+     */
+    protected Tuple next;
 
 
-	public MergeJoin(Iterator left, Iterator right, Integer lcol, Integer rcol){
-		schema = Schema.join(left.schema, right.schema);
-		this.left = left;
-		this.right =  right;
-		this.lcol = lcol;
-		this.rcol = rcol;
-		System.out.println("lcol = "+ lcol);
-		System.out.println("rcol = "+ rcol);
-		outer = null;
-		next = null;
-		
-	}
-	
+    public MergeJoin(Iterator left, Iterator right, Integer lcol, Integer rcol) {
+        this.left = left;
+        this.right = right;
+        this.lcol = lcol;
+        this.rcol = rcol;
+        schema = Schema.join(left.schema, right.schema);
 
-	@Override
-	public void explain(int depth) {
+    }
+
+
+    @Override
+    public void explain(int depth) {
+
+        indent(depth);
+        System.out.print("Projection : ");
+        for (int i = 0; i < this.schema.names.length - 1; i++) {
+            System.out.println("{" + this.schema.names[i] + "}");
+        }
+        System.out.println("{" + this.schema.names[this.schema.names.length - 1] + "}");
+        this.left.explain(depth + 1);
+        this.right.explain(depth + 1);
 //
-//		indent(depth);
-//		System.out.print("MergeJoin : ");
-//		if (.length > 0) {
-//			for (int i = 0; i < preds.length - 1; i++) {
-//				System.out.print(preds[i].toString() + " OR ");
-//			}
-//			System.out.println(preds[preds.length - 1]);
-//		} else {
-//			System.out.println("(cross)");
-//		}
-//		left.explain(depth + 1);
-//		right.explain(depth + 1);
-
-		// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 //		throw new UnsupportedOperationException("Not implemented");
-	}
+    }
 
-	@Override
-	public void restart() {
-		left.restart();
-		right.restart();
-		outer = null;
-		next = null;
-	}
+    @Override
+    public void restart() {
+        left.restart();
+        right.restart();
+        outer = null;
+        next = null;
+    }
 
-	@Override
-	public boolean isOpen() { return (left != null);
-	}
+    @Override
+    public boolean isOpen() {
+        return (left != null);
+    }
 
-	@Override
-	public void close() {
-		if (left != null) {
-			left.close();
-			right.close();
-			left = null;
-			right = null;
-		}
-	}
+    @Override
+    public void close() {
+        if (left != null) {
+            left.close();
+            right.close();
+            left = null;
+            right = null;
+        }
+    }
 
-	@Override
-	public boolean hasNext() {
-//		System.out.println("lcol = "+ lcol);
-//		System.out.println("rcol = "+ rcol);
-	while(true) {
-		if (outer == null) {
-//            System.out.println("outer = true");
-            if(left.hasNext()) {
-				outer = left.getNext();
-				lcol++;
-			}else{
-				return false;
-
-			}
-		}
-		if (this.right.hasNext()) {
-
-			this.next = this.right.getNext();
-			rcol++;
-//			System.out.println("joining "+outer.toString()+" with "+ right.toString());
-			this.next = Tuple.join(outer, right.getNext(), schema);
-			if(next.getField())
-			return true;
-			}
+    @Override
+    public boolean hasNext() {
+        while (true) {
+            if (outer == null) {
+                if (left.hasNext()) {
+                    outer = left.getNext();
+                } else {
+                    return false;
+                }
+            }
+            while (this.right.hasNext()) {
+                Tuple rightTuple = right.getNext();
+                next = Tuple.join(outer, rightTuple, schema);
+                if (outer.getField(lcol) == rightTuple.getField(rcol)) {
+                    return true;
+                }
+            }
 
 
-		outer = null;
-		lcol = 0;
-		right.restart();
-		}
+            outer = null;
+            right.restart();
+        }
 
 //		throw new IllegalStateException("debugging crash");
 
-	}
+    }
 
-	@Override
-	public Tuple getNext() {
+    @Override
+    public Tuple getNext() {
 
-		// validate the next tuple
-		if (next == null) {
-			throw new IllegalStateException("no more tuples");
-		}
-		// return (and forget) the tuple
-		Tuple tuple = next;
-		next = null;
-		return tuple;
-	}
+        // validate the next tuple
+        if (next == null) {
+            throw new IllegalStateException("no more tuples");
+        }
+        // return (and forget) the tuple
+        Tuple tuple = next;
+        next = null;
+        return tuple;
+    }
 
 }
