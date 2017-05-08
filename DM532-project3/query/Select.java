8636e8780cca258640f4a5ad5fe9f8cfbf7cb24e
Mark Jervelund <mark@jervelund.com>
2017-05-08 14:36:22 +0200

    Should be there+

--- DM532-project3/query/Select.java
+++ DM532-project3/query/Select.java
@@ -12,8 +12,8 @@ class Select implements Plan {
   protected Schema[] newschema;
   protected String[] columns;
   protected Predicate[][] preads;
-  public boolean j;
-  protected Iterator k;
+  public boolean isExplain;
+  protected Iterator interator;
   protected Schema schema;
   /**
    * Optimizes the plan, given the parsed query.
@@ -22,7 +22,7 @@ class Select implements Plan {
    */
   public Select(AST_Select tree) throws QueryException {
 
-    this.j = tree.isExplain;
+    this.isExplain = tree.isExplain;
     this.Selecter(tree);
     this.selectjoin();
 
@@ -54,25 +54,25 @@ class Select implements Plan {
   }
 
   protected void selectjoin() {
-    this.k = new FileScan(this.newschema[0], new HeapFile(this.tables[0]));
+    this.interator = new FileScan(this.newschema[0], new HeapFile(this.tables[0]));
 
     int var1;
     for(var1 = 1; var1 < this.tables.length; ++var1) {
-      this.k = new SimpleJoin(this.k, new FileScan(this.newschema[var1], new HeapFile(this.tables[var1])), new Predicate[0]);
+      this.interator = new SimpleJoin(this.interator, new FileScan(this.newschema[var1], new HeapFile(this.tables[var1])), new Predicate[0]);
     }
 
     for(var1 = 0; var1 < this.preads.length; ++var1) {
-      this.k = new Selection(this.k, this.preads[var1]);
+      this.interator = new Selection(this.interator, this.preads[var1]);
     }
 
     if(this.columns.length > 0) {
-      Integer[] var3 = new Integer[this.columns.length];
+      Integer[] intlist = new Integer[this.columns.length];
 
       for(int j = 0; j < this.columns.length; ++j) {
-        var3[j] = Integer.valueOf(this.schema.fieldNumber(this.columns[j]));
+        intlist[j] = Integer.valueOf(this.schema.fieldNumber(this.columns[j]));
       }
 
-      this.k = new Projection(this.k, var3);
+      this.interator = new Projection(this.interator, intlist);
     }
 
   }
@@ -81,10 +81,10 @@ class Select implements Plan {
    * Executes the plan and prints applicable output.
    */
   public void execute() {
-    if(this.j) {
-      this.k.explain(0);
+    if(isExplain) {
+      this.interator.explain(0);
     } else {
-      int var1 = this.k.execute();
+      int var1 = this.interator.execute();
       System.out.println("\n" + var1 + " rows affected.");
     }
   } // public void execute()
