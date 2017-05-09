8ccfc9331387858ceedcfe0f48984d04f07bc0c2
thesleort <troels@newtec.dk>
2017-05-09 11:04:35 +0200

    Updated variable names

--- DM532-project3/query/Update.java
+++ DM532-project3/query/Update.java
@@ -26,8 +26,8 @@ class Update implements Plan {
   public Update(AST_Update tree) throws QueryException {
     this.filename = tree.getFileName();
     this.schema = QueryCheck.tableExists(this.filename);
-    String[] var2 = tree.getColumns();
-    this.fldnos = QueryCheck.updateFields(this.schema, var2);
+    String[] columns = tree.getColumns();
+    this.fldnos = QueryCheck.updateFields(this.schema, columns);
     this.values = tree.getValues();
     QueryCheck.updateValues(this.schema, this.fldnos, this.values);
     this.preds = tree.getPredicates();
@@ -39,49 +39,49 @@ class Update implements Plan {
    * Executes the plan and prints applicable output.
    */
   public void execute() {
-    HeapFile var1 = new HeapFile(this.filename);
-    FileScan var2 = new FileScan(this.schema, var1);
-    Object var3 = var2;
+    HeapFile heap = new HeapFile(this.filename);
+    FileScan fileScan = new FileScan(this.schema, heap);
+    Object obj = fileScan;
 
     for(int var4 = 0; var4 < this.preds.length; ++var4) {
-      var3 = new Selection((Iterator)var3, this.preds[var4]);
+      obj = new Selection((Iterator)obj, this.preds[var4]);
     }
 
-    IndexDesc[] var13 = Minibase.SystemCatalog.getIndexes(this.filename, this.schema, this.fldnos);
-    int var5 = 0;
-    if(((Iterator)var3).hasNext()) {
-      Tuple var6 = ((Iterator)var3).getNext();
-      RID var7 = var2.getLastRID();
-//      IndexDesc[] var11 = var13;
-      int var9 = 0;
+    IndexDesc[] indexDescriptors = Minibase.SystemCatalog.getIndexes(this.filename, this.schema, this.fldnos);
+    int i = 0;
+    if(((Iterator)obj).hasNext()) {
+      Tuple tuple = ((Iterator)obj).getNext();
+      RID rid = fileScan.getLastRID();
+//      IndexDesc[] indexDecs2 = indexDescriptors;
+      int j = 0;
 
-      IndexDesc var8;
-      int var10;
-      SearchKey var12;
-      for(var10 = var13.length; var9 < var10; ++var9) {
-        var8 = var13[var9];
-        var12 = new SearchKey(var6.getField(var8.columnName));
-        (new HashIndex(var8.indexName)).deleteEntry(var12, var7);
+      IndexDesc indexDesc;
+      int l;
+      SearchKey key;
+      for(l = indexDescriptors.length; j < l; ++j) {
+        indexDesc = indexDescriptors[j];
+        key = new SearchKey(tuple.getField(indexDesc.columnName));
+        (new HashIndex(indexDesc.indexName)).deleteEntry(key, rid);
       }
 
-      for(int var14 = 0; var14 < this.fldnos.length; ++var14) {
-        var6.setField(this.fldnos[var14], this.values[var14]);
+      for(int k = 0; k < this.fldnos.length; ++k) {
+        tuple.setField(this.fldnos[k], this.values[k]);
       }
 
-      var1.updateRecord(var7, var6.getData());
-      ++var5;
-//      var11 = var13;
-      var9 = 0;
+      heap.updateRecord(rid, tuple.getData());
+      ++i;
+//      indexDecs2 = indexDescriptors;
+      j = 0;
 
-      for(var10 = var13.length; var9 < var10; ++var9) {
-        var8 = var13[var9];
-        var12 = new SearchKey(var6.getField(var8.columnName));
-        (new HashIndex(var8.indexName)).insertEntry(var12, var7);
+      for(l = indexDescriptors.length; j < l; ++j) {
+        indexDesc = indexDescriptors[j];
+        key = new SearchKey(tuple.getField(indexDesc.columnName));
+        (new HashIndex(indexDesc.indexName)).insertEntry(key, rid);
       }
     }
 
-    ((Iterator)var3).close();
-    System.out.println(var5 + " rows affected.");
+    ((Iterator)obj).close();
+    System.out.println(i + " rows affected.");
     // print the output message
     System.out.println("0 rows affected. (Not implemented)");
 
